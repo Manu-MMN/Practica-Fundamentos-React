789d59075a6aedfccea17846f9ccbf7bc986b869
Manu MMN <manu.mnez.n@gmail.com>
2025-02-16 23:54:26 +0100

    modificado Anuncio.jsx para informar si se vende o se compra

--- src/components/Anuncio.jsx
+++ src/components/Anuncio.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Anuncio = ({ anuncio, eliminarAnuncio}) =>{
+const Anuncio = ({ anuncio, eliminarAnuncio }) => {
     return (
         <div className="anuncio">
             <h2>{anuncio.name}</h2>
+            {anuncio.sale ? (
+                <p>Se vende</p> // Mostrar "Se vende" si sale es true
+            ) : (
+                <p>Se compra</p> // Mostrar "Se compra" si sale es false
+            )}
             <p>{anuncio.description}</p>
             <p>Precio: {anuncio.price} €</p>
             <button onClick={() => eliminarAnuncio(anuncio.id)}>Eliminar Anuncio</button>
